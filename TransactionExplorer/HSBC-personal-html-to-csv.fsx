4d6816d3dae9c41af17655fb747bae17a76a01ae
indy9000 <indy9000@gmail.com>
2015-04-05 01:04:10 +0100

    added fsharp formatting to generate html

--- TransactionExplorer/HSBC-personal-html-to-csv.fsx
+++ TransactionExplorer/HSBC-personal-html-to-csv.fsx
@@ -1,39 +1,69 @@
-﻿#r @"..\packages\FSharp.Data.2.2.0\lib\net40\FSharp.Data.dll"
+﻿(**
+HSBC html statement converted to CSV
+===============================
+*)
+
+(**  
+HSBC Personal Banking doesn't offer a way to download transactions in csv
+format. This F# script converts transactions found in a statement webpage to 
+csv. 
+
+How to use
+----------
+To use this, first login to personal banking and goto statements and save
+statement webpages as you navigate them. Then load this script and call
+*ConvertHtmlToCSV* with appropriate parameters
+*)
+
+#r @"../packages/FSharp.Data.2.2.0/lib/net40/FSharp.Data.dll"
+
 open FSharp.Data
 open System
 open System.IO
 
-(*  HSBC Personal Banking doesn't offer a way to download transactions in csv
-    format. This script converts transactions in a HSBC statement webpage to 
-    csv. 
-    To use this, first login to personal banking goto statements and save
-    statement webpages as you navigate them.
+(**
+Transactions are inside an html table. It looks as follows:
+``
+<table summary="This table contains a statement of your account">
+...
+</table>
+``
+We extract it using transacationTable 
 *)
-
 let transactionTable (html:HtmlDocument) = 
     html.Descendants ["table"]
-    |> Seq.choose(fun x->
-        x.TryGetAttribute("summary")
-        |> Option.map(fun a -> x, a.Value())
+    |> Seq.choose(fun n->
+        n.TryGetAttribute("summary")
+        |> Option.map(fun a -> n, a.Value())
     )
-    |> Seq.filter(fun (n,k)-> k.Contains("This table contains a statement of your account"))
-    |> Seq.map(fun (n,k) -> n)
+    |> Seq.filter(fun (n,av)-> av.Contains("This table contains a statement of your account"))
+    |> Seq.map(fun (n,av) -> n)
     |> Seq.head
 
+(**
+We extract the headers for csv from the thead columns
+*)
 let headers (table:HtmlNode) = 
-    table |> fun k-> k.Descendants ["thead"] |> Seq.head
+    table 
+    |> fun k-> k.Descendants ["thead"] |> Seq.head
     |> fun x -> x.Descendants["tr"] |> Seq.head
     |> fun x -> x.Descendants["th"] |> Seq.map(fun x -> x.InnerText().Trim())
 
+(**
+We extract the table body and extract each row
+*)
 let transactionRows (table:HtmlNode)=
-    table |> fun k-> k.Descendants ["tbody"] |> Seq.head
+    table 
+    |> fun k-> k.Descendants ["tbody"] |> Seq.head
     |> fun x -> x.Descendants["tr"]
     |> Seq.map(fun tr -> 
         tr.Descendants["td"] 
         |> Seq.map(fun x -> System.Net.WebUtility.HtmlDecode(x.InnerText().Trim()) )
     )
-
-//csv    
+(**
+we put all these together in the following that takes an html file and spits out
+a csv file.
+*)  
 let ConvertHtmlToCSV (html_file:string) (csv_file:string) =
     let html = HtmlDocument.Load(html_file)
     let table = transactionTable html
---
+++ TransactionExplorer/generate-html-docs.fsx
@@ -0,0 +1,18 @@
+﻿#load @"..\packages\FSharp.Formatting.2.8.0\FSharp.Formatting.fsx"
+open FSharp.Literate
+open System.IO
+
+// ----------------------------------------------------------------------------
+// SETUP
+// ----------------------------------------------------------------------------
+
+/// Return path relative to the current file location
+let relative subdir = Path.Combine(__SOURCE_DIRECTORY__, subdir)
+
+
+/// Processes a single F# Script file and produce HTML output
+let file = relative "HSBC-personal-html-to-csv.fsx"
+let output = relative "docs/HSBC-personal-html-to-csv.html"
+let template = relative @"../templates/template-file.html"
+Literate.ProcessScriptFile(file, template, output)
+
